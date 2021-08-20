95561d32bd4f87bfcea0c544f2cd969687854859
zjtypy <1746481077@qq.com>
2021-08-20 20:31:48 +0800

    添加Test01.java

---
+++ src/test/java/org/example/Test01.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @Author:ZjtCommander
+ * @Description:
+ * @Date:create 2021/8/20  20:30
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("test01...");
+    }
+}
