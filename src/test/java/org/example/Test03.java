4d0dfbf02de58d077bd5c3b9a054c4488e25822d
zjtypy <1746481077@qq.com>
2021-08-20 21:07:38 +0800

    C2客户端更新操作

---
+++ src/test/java/org/example/Test03.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * @Author:ZjtCommander
+ * @Description:
+ * @Date:create 2021/8/20  21:05
+ */
+public class Test03 {
+    public static void main(String[] args) {
+        System.out.println("test03...");
+        System.out.println("C2客户端提交test03...");
+    }
+}
