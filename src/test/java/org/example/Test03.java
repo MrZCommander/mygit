76e44249ec09c111ba6129c761476d66b0530149
zjtypy <1746481077@qq.com>
2021-08-20 21:04:53 +0800

    C1客户端操作

---
+++ src/test/java/org/example/Test03.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * @Author:ZjtCommander
+ * @Description:
+ * @Date:create 2021/8/20  21:03
+ */
+public class Test03 {
+    public static void main(String[] args) {
+        System.out.println("test03...");
+        System.out.println("C1客户端提交test03...");
+    }
+}
